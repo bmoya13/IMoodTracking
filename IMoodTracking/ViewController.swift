8222b60fab7ac1f9b82419ca8043013221e339c7
bmoya13 <bimoya@cpp.edu>
2021-11-29 13:31:12 -0800

    Initial Commit

---
+++ IMoodTracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMoodTracking
+//
+//  Created by bmoya1301 on 11/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
