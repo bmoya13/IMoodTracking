319af2cb94f33b40bf02a6fb128410d145051553
Jeremy Lesmana <jeremylesmana@gmail.com>
2021-11-30 16:45:33 -0800

    Added amazing button click

--- IMoodTracking/LogMoodController.swift
+++ IMoodTracking/LogMoodController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class LogMoodController: UIViewController {
 
+    @IBAction func amazingButtonPressed(_ sender: Any) {
+        print("Test")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
