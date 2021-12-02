4b6bc710936a9a11233f5a4601f9a4f831d7e8ac
bmoya13 <bimoya@cpp.edu>
2021-12-01 16:03:20 -0800

    added the userdef+ file actually this time.

---
+++ IMoodTracking/UserDef+.swift
@@ -0,0 +1,47 @@
+//
+//  UserDefaults+.swift
+//  UserDefaultsUIKitDemo
+//
+//  Created by Alex Nagy on 13/10/2020.
+//  CS4080.02 : Used Resource from online
+
+import UIKit
+
+protocol ObjectSaveable {
+    func setObject<Object>(_ object: Object, forKey: String) throws where Object: Encodable
+    func getObject<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable
+}
+
+enum ObjectSaveableError: String, LocalizedError {
+    case unableToEncode = "Unable to encode object into data"
+    case noValue = "No data object found for the given key"
+    case unableToDecode = "Unable to decode object into given type"
+    
+    var errorDescription: String? {
+        rawValue
+    }
+}
+
+extension UserDefaults: ObjectSaveable {
+    func setObject<Object>(_ object: Object, forKey: String) throws where Object: Encodable {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            set(data, forKey: forKey)
+        } catch {
+            throw ObjectSaveableError.unableToEncode
+        }
+    }
+    
+    func getObject<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable {
+        guard let data = data(forKey: forKey) else { throw ObjectSaveableError.noValue }
+        let decoder = JSONDecoder()
+        do {
+            let object = try decoder.decode(type, from: data)
+            return object
+        } catch {
+            throw ObjectSaveableError.unableToDecode
+        }
+    }
+}
+
